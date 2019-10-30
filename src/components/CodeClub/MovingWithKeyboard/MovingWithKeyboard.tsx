d46a38582dc98ac775bb5d703065e664d84594dd
Joe Sharp <joe.martin.sharp@gmail.com>
2019-10-30 22:27:59 +0000

    experiment with embedding codepen

--- src/components/CodeClub/MovingWithKeyboard/MovingWithKeyboard.tsx
+++ src/components/CodeClub/MovingWithKeyboard/MovingWithKeyboard.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const MovingWithKeyboard: React.FunctionComponent = () => {
-  return <h1>Moving With Keyboard</h1>;
+  return (
+    <div className="container">
+      <h1>Moving With Keyboard</h1>
+      <p>
+        We will be learning how to move shapes around the screen using our
+        keyboard. This will give you some options when writing games. Here is a
+        completed example.
+      </p>
+      <iframe
+        height="265"
+        style={{ width: "100%", height: "550px" }}
+        scrolling="no"
+        title="empty-example"
+        src="https://codepen.io/JoeSharpBalcarras/embed/zYYPYjP?height=265&theme-id=0&default-tab=js,result"
+      >
+        See the Pen{" "}
+        <a href="https://codepen.io/JoeSharpBalcarras/pen/zYYPYjP">
+          Moving with Keyboard
+        </a>{" "}
+        by Joe (
+        <a href="https://codepen.io/JoeSharpBalcarras">@JoeSharpBalcarras</a>)
+        on <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+    </div>
+  );
 };
 
 export default MovingWithKeyboard;
