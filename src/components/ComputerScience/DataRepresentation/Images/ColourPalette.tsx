bffc3a0374be2e3fb7f16f7036736f5c52e1b756
Joe Sharp <ratracejoe@hotmail.com>
2020-08-13 23:45:34 +0100

    genering greyscale

--- src/components/ComputerScience/DataRepresentation/Images/ColourPalette.tsx
+++ src/components/ComputerScience/DataRepresentation/Images/ColourPalette.tsx
@@ -28,9 +28,9 @@ const ColourPalette: React.FunctionComponent<Props> = ({
     <div className="colour-palette">
       <div className="colour-palette__selector">
         {availableColoursWithHandlers.map(
-          ({ backgroundColor, onClick, isCurrent }) => (
+          ({ backgroundColor, onClick, isCurrent }, i) => (
             <div
-              key={backgroundColor}
+              key={i}
               style={{ backgroundColor }}
               onClick={onClick}
               className={isCurrent ? "current" : ""}
@@ -58,79 +58,18 @@ export interface UseColourPallete {
   componentProps: Props;
 }
 
-//https://gist.github.com/ibrechin/2489005
-// Thanks matey!
-const hslToRgb = function (_h: number, s: number, l: number): string {
-  var h = Math.min(_h, 359) / 60;
-
-  var c = (1 - Math.abs(2 * l - 1)) * s;
-  var x = c * (1 - Math.abs((h % 2) - 1));
-  var m = l - 0.5 * c;
-
-  var r = m,
-    g = m,
-    b = m;
-
-  if (h < 1) {
-    r += c;
-    g = +x;
-    b += 0;
-  } else if (h < 2) {
-    r += x;
-    g += c;
-    b += 0;
-  } else if (h < 3) {
-    r += 0;
-    g += c;
-    b += x;
-  } else if (h < 4) {
-    r += 0;
-    g += x;
-    b += c;
-  } else if (h < 5) {
-    r += x;
-    g += 0;
-    b += c;
-  } else if (h < 6) {
-    r += c;
-    g += 0;
-    b += x;
-  } else {
-    r = 0;
-    g = 0;
-    b = 0;
-  }
-
-  return (
-    "rgb(" +
-    Math.floor(r * 255) +
-    ", " +
-    Math.floor(g * 255) +
-    ", " +
-    Math.floor(b * 255) +
-    ")"
-  );
-};
-
-const createSpectrum = function (length: number): string[] {
-  var colors: string[] = [];
-  // 270 because we don't want the spectrum to circle back
-  var step = 360 / length;
-  for (var i = 1; i <= length; i++) {
-    var color = hslToRgb(i * step, 0.5, 0.5);
-    colors.push(color);
-  }
-
-  return colors;
-};
-
 export const useColourPallete = ({
   colourDepth,
 }: UseProps): UseColourPallete => {
   // Work out available colours based on depth...
   const availableColours: ColourMap = React.useMemo(() => {
-    const spectrum = createSpectrum(Math.pow(2, colourDepth));
-    return spectrum.reduce((acc, curr, i) => ({ ...acc, [i]: curr }), {});
+    const numberColours = Math.pow(2, colourDepth);
+    const spectrum: ColourMap = {};
+    for (let x = 0; x < numberColours; x++) {
+      const c = Math.floor((255 * x) / numberColours);
+      spectrum[x] = `rgb(${c},${c},${255 - c})`;
+    }
+    return spectrum;
   }, [colourDepth]);
 
   const [value, onChange] = React.useState<number>(0);
