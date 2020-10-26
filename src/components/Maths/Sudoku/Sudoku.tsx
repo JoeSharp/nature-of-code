03981231a99c580b5452f25758b2c8508b125d26
Joe Sharp <ratracejoe@hotmail.com>
2020-10-26 13:37:13 +0000

    providing hints

--- src/components/Maths/Sudoku/Sudoku.tsx
+++ src/components/Maths/Sudoku/Sudoku.tsx
@@ -1,35 +1,97 @@
-import React from 'react';
+import React from "react";
 
-import useSudoku, { EMPTY_CELL, DIMENSION } from './useSudoku';
+import useSudoku, { EMPTY_CELL, DIMENSION } from "./useSudoku";
+import { Cell, CoordinateConsumer } from "./types";
 
-import './sudoku.css'
+import "./sudoku.css";
+
+interface CellProps {
+  cell: Cell;
+  x: number;
+  y: number;
+  setBoard: CoordinateConsumer;
+}
+
+const CellComponent: React.FunctionComponent<CellProps> = ({
+  x,
+  y,
+  cell,
+  setBoard,
+}) => {
+  let classNames: string[] = ["sudoku-cell"];
+  if (cell.error) {
+    classNames.push("sudoku-cell-error");
+  }
+  if (cell.value === EMPTY_CELL) {
+    classNames.push("sudoku-cell-hint");
+  }
+  return (
+    <td className={classNames.join(" ")} onClick={() => setBoard({ x, y })}>
+      {cell.value === EMPTY_CELL
+        ? Array.from(cell.allowed).join(", ")
+        : cell.value}
+    </td>
+  );
+};
+
+interface RowProps {
+  x: number;
+  row: Cell[];
+  value: number;
+  setBoard: CoordinateConsumer;
+}
+
+const RowComponent: React.FunctionComponent<RowProps> = ({
+  x,
+  row,
+  setBoard,
+  value,
+}) => (
+  <tr>
+    {row.map((cell, y) => (
+      <CellComponent key={y} {...{ x, y, cell, setBoard, value }} />
+    ))}
+  </tr>
+);
 
 const Sudoku: React.FunctionComponent = () => {
-    const [value, setValue] = React.useState<number>(5);
-    const { board, setBoard } = useSudoku();
+  const [value, setValue] = React.useState<number>(5);
+  const { board, setBoard: _setBoard } = useSudoku();
 
-    const onValueChange: React.ChangeEventHandler<HTMLInputElement> =
-        React.useCallback(({ target: { value } }) => setValue(parseInt(value)), [setValue]);
+  const onValueChange: React.ChangeEventHandler<HTMLInputElement> = React.useCallback(
+    ({ target: { value } }) => setValue(parseInt(value)),
+    [setValue]
+  );
 
-    return (
-        <div>
-            <form>
-                <div className='form-group'>
-                    <input className='form-control' min={EMPTY_CELL} max={DIMENSION} type='number' value={value} onChange={onValueChange} />
-                </div>
-            </form>
-            <table className='sudoku-table'>
-                <thead></thead>
-                <tbody>
-                    {board.map((row, x) => <tr key={x}>
-                        {row.map((cell, y) => <td key={y} className={cell.error ? 'sudoku-cell sudoku-cell-error' : 'sudoku-cell'} onClick={() => setBoard({ x, y, value })}>
-                            {cell.value === EMPTY_CELL ? '' : cell.value}
-                        </td>)}
-                    </tr>)}
+  const setBoard: CoordinateConsumer = React.useCallback(
+    ({ x, y }) => _setBoard({ x, y, value }),
+    [value, _setBoard]
+  );
 
-                </tbody>
-            </table>
-        </div>)
-}
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <input
+            className="form-control"
+            min={EMPTY_CELL}
+            max={DIMENSION}
+            type="number"
+            value={value}
+            onChange={onValueChange}
+          />
+        </div>
+      </form>
+      <table className="sudoku-table">
+        <thead></thead>
+        <tbody>
+          {board.map((row, x) => (
+            <RowComponent key={x} {...{ x, setBoard, row, value }} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Sudoku;
--- src/components/Maths/Sudoku/useSudoku.ts
+++ src/components/Maths/Sudoku/useSudoku.ts
@@ -1,158 +1,177 @@
-import React from 'react';
+import React from "react";
+
+import {
+  Coordinate,
+  GetOtherCellsFunction,
+  BoardActionConsumer,
+  BoardAction,
+  BoardState,
+} from "./types";
 
 export const EMPTY_CELL = -1;
 export const SUB_DIMENSION = 3;
 export const DIMENSION = Math.pow(SUB_DIMENSION, 2);
+export const DIGITS = Array(DIMENSION)
+  .fill(null)
+  .map((_, i) => i + 1);
 
-interface Cell {
-    fixed: boolean;
-    value: number,
-    error: boolean
-}
-
-type BoardState = Cell[][];
-
-interface Coordinate {
-    x: number;
-    y: number;
-}
-
-interface BoardAction extends Coordinate {
-    value: number;
-    fix?: boolean;
-}
-
-type GetOtherCellsFunction = (x: number, y: number) => Coordinate[];
-
-const getRowCells: GetOtherCellsFunction = 
-    (x: number, y: number): Coordinate[] => Array(DIMENSION).fill(null).map((_, i) => ({x: i, y}))
-const getColCells: GetOtherCellsFunction = 
-    (x: number, y: number): Coordinate[] => Array(DIMENSION).fill(null).map((_, i) => ({x, y: i}))
-const getSquareCells: GetOtherCellsFunction =
-    (x: number, y: number): Coordinate[] => {
-        let coords: Coordinate[] = [];
-        let xFloor = Math.floor(x / SUB_DIMENSION) * SUB_DIMENSION;
-        let yFloor = Math.floor(y / SUB_DIMENSION) * SUB_DIMENSION;
-        for (let xi=0; xi<SUB_DIMENSION; xi++) {
-            for (let yi=0; yi<SUB_DIMENSION; yi++) {
-                coords.push({
-                    x: xFloor + xi,
-                    y: yFloor + yi
-                })
-            }  
-        }
-        return coords;
+const getRowCells: GetOtherCellsFunction = (
+  x: number,
+  y: number
+): Coordinate[] =>
+  Array(DIMENSION)
+    .fill(null)
+    .map((_, i) => ({ x: i, y }));
+const getColCells: GetOtherCellsFunction = (
+  x: number,
+  y: number
+): Coordinate[] =>
+  Array(DIMENSION)
+    .fill(null)
+    .map((_, i) => ({ x, y: i }));
+const getSquareCells: GetOtherCellsFunction = (
+  x: number,
+  y: number
+): Coordinate[] => {
+  let coords: Coordinate[] = [];
+  let xFloor = Math.floor(x / SUB_DIMENSION) * SUB_DIMENSION;
+  let yFloor = Math.floor(y / SUB_DIMENSION) * SUB_DIMENSION;
+  for (let xi = 0; xi < SUB_DIMENSION; xi++) {
+    for (let yi = 0; yi < SUB_DIMENSION; yi++) {
+      coords.push({
+        x: xFloor + xi,
+        y: yFloor + yi,
+      });
     }
+  }
+  return coords;
+};
 const getOtherCellFunctions: GetOtherCellsFunction[] = [
-    getRowCells,
-    getColCells,
-    getSquareCells
-]
+  getRowCells,
+  getColCells,
+  getSquareCells,
+];
 
 const reducer = (state: BoardState, action: BoardAction): BoardState => {
-    const newState = state.map((col) => col.map(c => ({...c})));
+  // Make a complete deep copy of the board
+  const newState = state.map((col) => col.map((c) => ({ ...c })));
 
-    for (let x = 0; x < DIMENSION; x++) {
-        for (let y = 0; y < DIMENSION; y++) {
-            if ((x === action.x) && (y === action.y)) {
-                if (action.fix || !newState[x][y].fixed) {
-                    if (newState[x][y].value === action.value) {
-                        newState[x][y].value = EMPTY_CELL;
-                    } else {
-                        newState[x][y].value = action.value;
-                        newState[x][y].fixed = action.fix || false;
-                    }
+  // Use this action to change the appropriate cell
+  for (let x = 0; x < DIMENSION; x++) {
+    for (let y = 0; y < DIMENSION; y++) {
+      if (x === action.x && y === action.y) {
+        if (action.fix || !newState[x][y].fixed) {
+          if (newState[x][y].value === action.value) {
+            newState[x][y].value = EMPTY_CELL;
+          } else {
+            newState[x][y].value = action.value;
+            newState[x][y].fixed = action.fix || false;
+          }
+        }
+      }
+
+      newState[x][y].error = false;
+    }
+  }
+
+  // Check each cell
+  for (let x = 0; x < DIMENSION; x++) {
+    for (let y = 0; y < DIMENSION; y++) {
+      let allowed = new Set(DIGITS);
+      getOtherCellFunctions.forEach((f) => {
+        let found: Set<number> = new Set();
+        let otherCells = f(x, y);
+        otherCells.forEach(({ x: xi, y: yi }) => {
+          if (newState[x][y].value !== EMPTY_CELL) {
+            if (
+              found.has(newState[xi][yi].value) &&
+              newState[xi][yi].value !== EMPTY_CELL
+            ) {
+              otherCells.forEach(({ x: xj, y: yj }) => {
+                if (newState[xj][yj].value === newState[xi][yi].value) {
+                  newState[xj][yj].error = true;
                 }
+              });
             }
+            found.add(newState[xi][yi].value);
+          } else {
+            allowed.delete(newState[xi][yi].value);
+          }
+        });
+      });
 
-            newState[x][y].error = false;
-        }
+      //   console.log("Allowed...", { x, y, allowed });
+      newState[x][y].allowed = new Set(allowed);
     }
+  }
 
-    // Check each cell
-    for (let x = 0; x < DIMENSION; x++) {
-        for (let y = 0; y < DIMENSION; y++) {
-            if (newState[x][y].value !== EMPTY_CELL) {
-                getOtherCellFunctions.forEach(f => {
-                    let found: Set<number> = new Set();
-                    let otherCells = f(x, y);
-                    otherCells.forEach(({x:xi, y:yi}) => {
-                        if (found.has(newState[xi][yi].value)) {
-                            otherCells.forEach(({x: xj, y: yj}) => {
-                               if (newState[xj][yj].value === newState[xi][yi].value) {
-                                    newState[xj][yj].error = true;
-                                }
-                            })
-                        }
-                        found.add(newState[xi][yi].value)
-                    })
-
-                })
-            }
-        }
-    }
-
-    return newState;
-}
+  return newState;
+};
 
 const defaultState: BoardState = Array(DIMENSION)
-    .fill(null)
-    .map(i =>
-        Array(DIMENSION)
-            .fill(null)
-            .map(j => ({ value: EMPTY_CELL, error: false, fixed: false })));
+  .fill(null)
+  .map((i) =>
+    Array(DIMENSION)
+      .fill(null)
+      .map((j) => ({
+        value: EMPTY_CELL,
+        error: false,
+        fixed: false,
+        allowed: new Set(DIGITS),
+      }))
+  );
 
 const initialActions: BoardAction[] = [
-    { x: 0, y: 0, value: 6 },
-    { x: 0, y: 1, value: 2 },
-    { x: 0, y: 8, value: 1 },
-    { x: 1, y: 2, value: 4 },
-    { x: 1, y: 3, value: 7 },
-    { x: 1, y: 5, value: 1 },
-    { x: 1, y: 6, value: 6 },
-    { x: 1, y: 7, value: 9 },
-    { x: 2, y: 1, value: 1 },
-    { x: 2, y: 3, value: 3 },
-    { x: 2, y: 4, value: 6 },
-    { x: 2, y: 7, value: 8 },
-    { x: 3, y: 0, value: 3 },
-    { x: 3, y: 3, value: 8 },
-    { x: 3, y: 5, value: 6 },
-    { x: 3, y: 6, value: 9 },
-    { x: 4, y: 3, value: 9 },
-    { x: 4, y: 4, value: 4 },
-    { x: 4, y: 5, value: 3 },
-    { x: 5, y: 2, value: 6 },
-    { x: 5, y: 3, value: 1 },
-    { x: 5, y: 5, value: 2 },
-    { x: 5, y: 8, value: 5 },
-    { x: 6, y: 1, value: 9 },
-    { x: 6, y: 4, value: 3 },
-    { x: 6, y: 5, value: 8 },
-    { x: 6, y: 7, value: 1 },
-    { x: 7, y: 1, value: 3 },
-    { x: 7, y: 2, value: 5 },
-    { x: 7, y: 3, value: 6 },
-    { x: 7, y: 5, value: 7 },
-    { x: 7, y: 6, value: 4 },
-    { x: 8, y: 0, value: 1 },
-    { x: 8, y: 7, value: 5 },
-    { x: 8, y: 8, value: 8 },
-]
+  { x: 0, y: 0, value: 6 },
+  { x: 0, y: 1, value: 2 },
+  { x: 0, y: 8, value: 1 },
+  { x: 1, y: 2, value: 4 },
+  { x: 1, y: 3, value: 7 },
+  { x: 1, y: 5, value: 1 },
+  { x: 1, y: 6, value: 6 },
+  { x: 1, y: 7, value: 9 },
+  { x: 2, y: 1, value: 1 },
+  { x: 2, y: 3, value: 3 },
+  { x: 2, y: 4, value: 6 },
+  { x: 2, y: 7, value: 8 },
+  { x: 3, y: 0, value: 3 },
+  { x: 3, y: 3, value: 8 },
+  { x: 3, y: 5, value: 6 },
+  { x: 3, y: 6, value: 9 },
+  { x: 4, y: 3, value: 9 },
+  { x: 4, y: 4, value: 4 },
+  { x: 4, y: 5, value: 3 },
+  { x: 5, y: 2, value: 6 },
+  { x: 5, y: 3, value: 1 },
+  { x: 5, y: 5, value: 2 },
+  { x: 5, y: 8, value: 5 },
+  { x: 6, y: 1, value: 9 },
+  { x: 6, y: 4, value: 3 },
+  { x: 6, y: 5, value: 8 },
+  { x: 6, y: 7, value: 1 },
+  { x: 7, y: 1, value: 3 },
+  { x: 7, y: 2, value: 5 },
+  { x: 7, y: 3, value: 6 },
+  { x: 7, y: 5, value: 7 },
+  { x: 7, y: 6, value: 4 },
+  { x: 8, y: 0, value: 1 },
+  { x: 8, y: 7, value: 5 },
+  { x: 8, y: 8, value: 8 },
+];
 
 interface UseSudoku {
-    board: BoardState;
-    setBoard: (action: BoardAction) => void;
+  board: BoardState;
+  setBoard: BoardActionConsumer;
 }
 
 const useSudoku = (): UseSudoku => {
-    const [board, setBoard] = React.useReducer(reducer, defaultState);
+  const [board, setBoard] = React.useReducer(reducer, defaultState);
 
-    React.useEffect(() => {
-        initialActions.map(i=>({...i, fix: true})).forEach(setBoard)
-    }, [])
+  React.useEffect(() => {
+    initialActions.map((i) => ({ ...i, fix: true })).forEach(setBoard);
+  }, []);
 
-    return { board, setBoard }
-}
+  return { board, setBoard };
+};
 
 export default useSudoku;
