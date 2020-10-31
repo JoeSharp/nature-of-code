7c8c99db091fb262a9b39ffa11fd0ea3bf30e2bf
Joe Sharp <ratracejoe@hotmail.com>
2020-10-31 12:24:41 +0000

    added ability to provide answers and it marks them

--- src/components/Maths/NegativeNumbers/NegativeNumbers.tsx
+++ src/components/Maths/NegativeNumbers/NegativeNumbers.tsx
@@ -6,27 +6,37 @@ import ArithmeticOperationPicker, {
 
 import "./quizzes.css";
 
+const Tick: React.FunctionComponent = () =>
+  (<svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="green" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
+  </svg>);
+
+const Exclamation: React.FunctionComponent = () =>
+  (<svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-exclamation-square" fill="red" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+    <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+  </svg>)
+
 const NegativeNumbers: React.FunctionComponent = () => {
   const { componentProps, operation } = usePicker("form-control");
 
-  const { quiz: quiz1, regenerateQuiz: regenerateQuiz1 } = useQuiz({
+  const quiz1 = useQuiz({
     partRange: 20,
     numberParts: 2,
     numberQuestions: 10,
     operation,
   });
-  const { quiz: quiz2, regenerateQuiz: regenerateQuiz2 } = useQuiz({
+  const quiz2 = useQuiz({
     partRange: 30,
     numberParts: 3,
     numberQuestions: 10,
     operation,
   });
 
-  const quizzes = [quiz1, quiz2];
-  const regenerateQuizzes = React.useCallback(() => {
-    regenerateQuiz1();
-    regenerateQuiz2();
-  }, [regenerateQuiz1, regenerateQuiz2]);
+  const quizzes = React.useMemo(() => [quiz1, quiz2], [quiz1, quiz2]);
+  const regenerateQuizzes = React.useCallback(() =>
+    quizzes.forEach(({ regenerateQuiz }) => regenerateQuiz())
+    , [quizzes]);
 
   let [revealAnswers, toggleRevealAnswer] = React.useReducer((a) => !a, false);
 
@@ -48,14 +58,18 @@ const NegativeNumbers: React.FunctionComponent = () => {
           <div>
             <table>
               <tbody>
-                {quiz.map(({ question, answer }, questionI) => (
+                {quiz.questions.map(({ question, givenAnswer = '0', answer }, questionI) => (
                   <tr>
                     <td className="questionIndex">
                       {quizI + 1}.{questionI + 1})
                     </td>
                     <td>{question}</td>
                     <td>=</td>
-                    <td>{revealAnswers ? answer : "?"}</td>
+                    <td>{revealAnswers ? <span>{answer} {answer === givenAnswer ? <Tick /> : <Exclamation />}</span> :
+                      <input type='number'
+                        className='txtGiveAnswer'
+                        value={givenAnswer}
+                        onChange={e => quiz.giveAnswer(questionI, parseInt(e.target.value))} />}</td>
                   </tr>
                 ))}
               </tbody>
